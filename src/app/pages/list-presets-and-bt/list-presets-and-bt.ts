38f99e2b6f4debf9d207eaeb9ff3ac02948eeddd
unknown <glaubermonteiro01@gmail.com>
2025-08-13 19:34:05 -0300

    List w table and pagination

--- src/app/pages/list-presets-and-bt/list-presets-and-bt.ts
+++ src/app/pages/list-presets-and-bt/list-presets-and-bt.ts
@@ -1,19 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { BackingTracksService } from '../../services/backingtrack.service';
 import { BackingTrack } from '../../models/BackingTrack';
 
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule, MatPaginator } from '@angular/material/paginator';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableDataSource } from '@angular/material/table';
+
 @Component({
   selector: 'app-list-presets-and-bt',
   templateUrl: './list-presets-and-bt.html',
   standalone: true,
-  imports: [CommonModule, HttpClientModule]
+  imports: [
+    CommonModule,
+    HttpClientModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatProgressSpinnerModule,
+    MatButtonModule
+  ]
 })
 export class ListPresetsAndBt implements OnInit {
   presets: BackingTrack[] = [];
   loading = false;
   error = '';
+  
+  displayedColumns: string[] = ['artist', 'song', 'playmusic', 'presetguitar', 'software', 'videopreset'];
+  dataSource = new MatTableDataSource<BackingTrack>();
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
 
   constructor(private backingTracksService: BackingTracksService) {}
 
@@ -21,9 +39,14 @@ export class ListPresetsAndBt implements OnInit {
     this.loading = true;
     this.backingTracksService.getBackingTracks().subscribe({
       next: (data) => {
-        console.log('Dados recebidos da API:', data);
         this.presets = data;
+        this.dataSource.data = data;
         this.loading = false;
+
+        // Configura o paginator
+        setTimeout(() => { 
+          if(this.paginator) this.dataSource.paginator = this.paginator;
+        });
       },
       error: (err) => {
         this.error = 'Erro ao carregar dados.';
