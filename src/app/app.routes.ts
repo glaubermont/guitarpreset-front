a0b20c0ba6fc92921d052841ea20afb736a21242
unknown <glaubermonteiro01@gmail.com>
2025-08-17 14:21:41 -0300

    path coringa

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,5 +3,6 @@ import { ListPresetsAndBt } from './pages/list-presets-and-bt/list-presets-and-b
 
 export const routes: Routes = [
   { path: '', redirectTo: 'list-presets-and-bt', pathMatch: 'full' },
-  { path: 'list-presets-and-bt', component: ListPresetsAndBt }
+  { path: 'list-presets-and-bt', component: ListPresetsAndBt },
+   { path: '**', redirectTo: 'list-presets-and-bt' }
 ];
