d36828a90e2058f60bc2454216302c3cecd4b39b
unknown <glaubermonteiro01@gmail.com>
2025-08-18 18:29:50 -0300

    components menu

---
+++ src/app/pages/download-preset/download-preset.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-download-preset',
+  imports: [],
+  templateUrl: './download-preset.html',
+  styleUrl: './download-preset.scss'
+})
+export class DownloadPreset {
+
+}
---
+++ src/app/pages/upload-preset/upload-preset.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-upload-preset',
+  imports: [],
+  templateUrl: './upload-preset.html',
+  styleUrl: './upload-preset.scss'
+})
+export class UploadPreset {
+
+}
