34c4dd3a7c05611953a3c68a1bd34c07fd18765d
unknown <glaubermonteiro01@gmail.com>
2025-08-18 18:21:21 -0300

    modal working video testing preset

--- src/app/pipes/safe.pipe.ts
+++ src/app/pipes/safe.pipe.ts
@@ -4,7 +4,12 @@ import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 @Pipe({name: 'safe', standalone: true})
 export class SafePipe implements PipeTransform {
   constructor(private sanitizer: DomSanitizer) {}
+
   transform(url: string): SafeResourceUrl {
+    // Converte URL do YouTube para embed
+    if (url.includes('youtube.com/watch')) {
+      url = url.replace('watch?v=', 'embed/');
+    }
     return this.sanitizer.bypassSecurityTrustResourceUrl(url);
   }
 }
