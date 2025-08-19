89b5ea8851f922cbc27d0b732d91a482265eab00
unknown <glaubermonteiro01@gmail.com>
2025-08-19 20:40:27 -0300

    vercel config

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,4 @@
 import { Routes } from '@angular/router';
-import { ListPresetsAndBt } from './pages/list-presets-and-bt/list-presets-and-bt';
-import { UploadPreset } from './pages/upload-preset/upload-preset';
-import { DownloadPreset } from './pages/download-preset/download-preset';
-import { Terms } from './pages/terms/terms';
-import { Contact } from './pages/contact/contact';
 
 export const routes: Routes = [
   { 
@@ -16,11 +11,12 @@ export const routes: Routes = [
     loadComponent: () => import('./pages/upload-preset/upload-preset')
       .then(m => m.UploadPreset)
   },
-  { 
-    path: 'download-preset/:id',
-    loadComponent: () => import('./pages/download-preset/download-preset')
-      .then(m => m.DownloadPreset),
-      data: { renderMode: 'ssr' }  // indica que essa rota será renderizada no servidor
+  {
+  path: 'download-preset/:id',
+  loadComponent: () => import('./pages/download-preset/download-preset')
+    .then(m => m.DownloadPreset),  // ← vírgula aqui
+  data: { prerender: false }      // desabilita prerender
+  
   },
   { 
     path: 'terms', 
@@ -37,3 +33,4 @@ export const routes: Routes = [
     redirectTo: 'list-presets-and-bt' 
   }
 ];
+
