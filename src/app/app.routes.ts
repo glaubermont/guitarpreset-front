716abbf7c5769563032a46103d70d2541ddae2bf
unknown <glaubermonteiro01@gmail.com>
2025-08-20 20:57:51 -0300

    home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,12 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
+
+   { 
+    path: '', 
+    loadComponent: () => import('./pages/list-presets-and-bt/list-presets-and-bt')
+      .then(m => m.ListPresetsAndBt)
+  },
   { 
     path: 'list-presets-and-bt', 
     loadComponent: () => import('./pages/list-presets-and-bt/list-presets-and-bt')
