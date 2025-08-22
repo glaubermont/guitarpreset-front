dad675189773e9971abb050fce0d0561bb8a4169
unknown <glaubermonteiro01@gmail.com>
2025-08-22 08:59:28 -0300

    no cache to endpoint findById

--- src/app/services/backingtrack.service.ts
+++ src/app/services/backingtrack.service.ts
@@ -17,11 +17,15 @@ export class BackingTracksService {
   }
 
   // Nova rota: buscar por ID
-  getBackingTrackById(id: number): Observable<BackingTrack> {
-    return this.http.get<BackingTrack>(`${this.apiUrl}/${id}`,{
-      headers: { 'Cache-Control': 'no-cache', 'Pragma': 'no-cache'}
-      });
-  }
+getBackingTrackById(id: number): Observable<BackingTrack> {
+  return this.http.get<BackingTrack>(`${this.apiUrl}/${id}`, {
+    headers: { 
+      'Cache-Control': 'no-cache, no-store, must-revalidate',
+      'Pragma': 'no-cache',
+      'Expires': '0'
+    }
+  });
+}
 
   downloadPreset(filename: string): Observable<Blob> {
     // Aqui está a correção:
